5237b1bdc4c1c29a30ce6d8fdec7c631704ccfb7
moch-alam <ddiaphragms@gmail.com>
2024-06-07 21:21:28 +0700

    Fitur Search Dari Jane

--- lib/app/modules/stock/views/stock_view.dart
+++ lib/app/modules/stock/views/stock_view.dart
@@ -33,113 +33,127 @@ class StockView extends GetView<StockController> {
             ),
           ),
           Expanded(
-            child: StreamBuilder<QuerySnapshot>(
-              stream: controller.streamData,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (snapshot.hasError) {
-                  return const Center(
-                    child: Text(
-                      'Error fetching data',
-                      style: TextStyle(color: Colors.red),
-                    ),
-                  );
-                } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                  return const Center(
-                    child: Text(
-                      'No data',
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                  );
-                } else {
-                  var data = snapshot.data!.docs;
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: data.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          color:
-                              const Color(0xFF478755), // Background color green
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 4.0, horizontal: 8.0),
-                        child: ListTile(
-                          title: Text(
-                            data[index]['nama'],
-                            style: const TextStyle(color: Colors.white),
+            child: Obx(() {
+              return StreamBuilder<QuerySnapshot>(
+                stream: controller.searchQuery.isEmpty
+                    ? controller.streamData
+                    : controller.search(controller.searchQuery.value),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (snapshot.hasError) {
+                    return const Center(
+                      child: Text(
+                        'Error fetching data',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    );
+                  } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                    return const Center(
+                      child: Text(
+                        'No data',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    );
+                  } else {
+                    var data = snapshot.data!.docs;
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        var doc = data[index];
+
+                        return Container(
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF478755),
+                            borderRadius: BorderRadius.circular(8.0),
                           ),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Stock: ${data[index]['stock']}',
-                                style: const TextStyle(
-                                    color: Color.fromARGB(221, 247, 255, 183)),
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 4.0, horizontal: 8.0),
+                          child: ListTile(
+                            title: Text(
+                              doc['nama'],
+                              style: const TextStyle(
+                                color: Colors.white,
                               ),
-                              Text(
-                                'Harga: ${data[index]['harga']}',
-                                style: const TextStyle(color: Colors.white),
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  ElevatedButton(
-                                    onPressed: () => Get.toNamed(Routes.UPDATE,
-                                        arguments: data[index].id),
-                                    style: ElevatedButton.styleFrom(
-                                      foregroundColor: Colors.white,
-                                      backgroundColor:
-                                          Colors.green, // Text color white
-                                    ),
-                                    child: const Text(
-                                      'Edit',
-                                      style: TextStyle(color: Colors.white),
-                                    ),
+                            ),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Stock: ${doc['stock']}',
+                                  style: const TextStyle(
+                                    color: Color.fromARGB(221, 255, 244, 143),
                                   ),
-                                  const SizedBox(width: 8),
-                                  ElevatedButton(
-                                    onPressed: () {
-                                      int hargaSatuan =
-                                          int.parse(data[index]['harga']);
-                                      int totalHargaBarang = hargaSatuan * 1;
-                                      controller.tambahPenjualan(
-                                          data[index]['nama'],
-                                          1,
-                                          hargaSatuan,
-                                          totalHargaBarang);
-                                    },
-                                    style: ElevatedButton.styleFrom(
-                                      foregroundColor: Colors.white,
-                                      backgroundColor:
-                                          Colors.green, // Text color white
-                                    ),
-                                    child: const Text(
-                                      'Penjualan',
-                                      style: TextStyle(color: Colors.white),
-                                    ),
+                                ),
+                                Text(
+                                  'Harga: ${doc['harga']}',
+                                  style: const TextStyle(
+                                    color: Color.fromARGB(221, 247, 255, 248),
                                   ),
-                                ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    ElevatedButton(
+                                      onPressed: () => Get.toNamed(
+                                          Routes.UPDATE,
+                                          arguments: data[index].id),
+                                      style: ElevatedButton.styleFrom(
+                                        foregroundColor: Colors.white,
+                                        backgroundColor:
+                                            Colors.green, // Text color white
+                                      ),
+                                      child: const Text(
+                                        'Edit',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                    const SizedBox(width: 8),
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        int hargaSatuan =
+                                            int.parse(data[index]['harga']);
+                                        int totalHargaBarang = hargaSatuan * 1;
+                                        controller.tambahPenjualan(
+                                            data[index]['nama'],
+                                            1,
+                                            hargaSatuan,
+                                            totalHargaBarang);
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        foregroundColor: Colors.white,
+                                        backgroundColor:
+                                            Colors.green, // Text color white
+                                      ),
+                                      child: const Text(
+                                        'Penjualan',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            trailing: IconButton(
+                              onPressed: () {
+                                controller.deleteData(doc.id);
+                              },
+                              icon: const Icon(
+                                Icons.delete_forever_rounded,
+                                color: Colors.white,
                               ),
-                            ],
+                            ),
                           ),
-                          trailing: IconButton(
-                            onPressed: () =>
-                                controller.deleteData(data[index].id),
-                            icon: const Icon(Icons.delete),
-                            color: Colors.white,
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                }
-              },
-            ),
+                        );
+                      },
+                    );
+                  }
+                },
+              );
+            }),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15.0),
@@ -167,7 +181,6 @@ class StockView extends GetView<StockController> {
                 ),
               ),
             ),
-            
           ),
           const Padding(padding: EdgeInsets.only(bottom: 3)),
         ],
