f877ce858d73aca4d6e046acd84f4cf3532e2467
jane <janenurfajriah@gmail.com>
2024-06-16 00:57:12 +0700

    nih udah bisa print satu hari

--- lib/app/modules/history/controllers/history_controller.dart
+++ lib/app/modules/history/controllers/history_controller.dart
@@ -1,13 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
 
 class HistoryController extends GetxController {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+  Rx<DateTime?> selectedDate = Rx<DateTime?>(null);
+
   Stream<QuerySnapshot<Object?>> streamDataRiwayat() {
     CollectionReference data = firestore.collection('riwayat');
-    return data.orderBy('tanggal', descending: true).snapshots();
+    Query query = data.orderBy('tanggal', descending: true);
+
+    if (selectedDate.value != null) {
+      int startOfDay = DateTime(selectedDate.value!.year,
+              selectedDate.value!.month, selectedDate.value!.day)
+          .millisecondsSinceEpoch;
+      int endOfDay = DateTime(selectedDate.value!.year,
+              selectedDate.value!.month, selectedDate.value!.day + 1)
+          .millisecondsSinceEpoch;
+      query = query
+          .where('tanggal', isGreaterThanOrEqualTo: startOfDay)
+          .where('tanggal', isLessThan: endOfDay);
+    }
+
+    return query.snapshots();
   }
 
   String formatCurrency(int amount) {
@@ -20,6 +39,45 @@ class HistoryController extends GetxController {
     DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(tgl);
     String formattedDate = DateFormat('dd MMMM yyyy', 'id_ID').format(dateTime);
     return formattedDate;
+  }
+
+  Future<void> printPdf(List<QueryDocumentSnapshot<Object?>> data) async {
+    final pdf = pw.Document();
+
+    pdf.addPage(
+      pw.MultiPage(
+        build: (pw.Context context) => [
+          pw.Center(
+            child: pw.Text(
+              'Invoice',
+              style: pw.TextStyle(fontSize: 24, fontWeight: pw.FontWeight.bold),
+            ),
+          ),
+          pw.ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              var document = data[index];
+              return pw.Container(
+                padding: pw.EdgeInsets.all(15),
+                child: pw.Column(
+                  crossAxisAlignment: pw.CrossAxisAlignment.start,
+                  children: [
+                    pw.Text("Pelanggan: ${document['pelanggan']}"),
+                    pw.Text(
+                        "Total Harga: ${formatCurrency(document['total_harga'])}"),
+                    pw.Text("Tanggal: ${formatDate(document['tanggal'])}"),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+
+    await Printing.layoutPdf(
+      onLayout: (PdfPageFormat format) async => pdf.save(),
+    );
   }
 
   @override
--- lib/app/modules/history/views/history_view.dart
+++ lib/app/modules/history/views/history_view.dart
@@ -1,96 +1,173 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
-import '../../../routes/app_pages.dart';
+import 'package:intl/intl.dart';
+import 'package:mystock/app/routes/app_pages.dart';
 import '../controllers/history_controller.dart';
 
 class HistoryView extends GetView<HistoryController> {
   const HistoryView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final HistoryController controller = Get.put(HistoryController());
+
+    void _selectDate(BuildContext context) async {
+      final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2101),
+      );
+      if (picked != null) {
+        controller.selectedDate.value = picked;
+      }
+    }
+
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('HistoryView'),
-      //   centerTitle: true,
-      // ),
-
       body: Container(
-          decoration: BoxDecoration(
-            color: Colors.grey[200], // Set the background color here
-          ),
-          child: StreamBuilder<QuerySnapshot<Object?>>(
-              stream: controller.streamDataRiwayat(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.active) {
-                  if (snapshot.data!.size != 0) {
-                    var data = snapshot.data!.docs;
-                    return ListView.builder(
-                      itemCount: data.length,
-                      itemBuilder: (context, index) {
-                        var document = data[index];
+        decoration: BoxDecoration(
+          color: Colors.grey[200], // Updated to a cleaner background color
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: Obx(() {
+                return StreamBuilder<QuerySnapshot<Object?>>(
+                  stream: controller.streamDataRiwayat(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.active) {
+                      if (snapshot.data!.size != 0) {
+                        var data = snapshot.data!.docs;
+                        return ListView.builder(
+                          itemCount: data.length,
+                          itemBuilder: (context, index) {
+                            var document = data[index];
 
-                        return Container(
-                          margin: const EdgeInsets.symmetric(
-                              vertical: 8.0, horizontal: 12.0),
-                          decoration: BoxDecoration(
-                            boxShadow: [
-                              BoxShadow(
-                                color: const Color.fromARGB(255, 0, 0, 0)
-                                    .withOpacity(0.5),
-                                spreadRadius: 2,
-                                blurRadius: 5,
-                                offset:
-                                    Offset(0, 3), // changes position of shadow
+                            return Container(
+                              margin: const EdgeInsets.symmetric(
+                                  vertical: 8.0, horizontal: 16.0),
+                              decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 2,
+                                    blurRadius: 5,
+                                    offset: Offset(0, 3),
+                                  ),
+                                ],
+                                color: Color.fromARGB(255, 70, 211,
+                                    91), // Updated background color
+                                borderRadius: BorderRadius.circular(15),
                               ),
-                            ],
-                            color: Color.fromARGB(255, 70, 211, 91),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-
-                          // ========================== ini di aktifkan nanti
-                          child: ListTile(
-                            onTap: () => Get.toNamed(Routes.HISTORY_INFO,
-                                arguments: document.id),
-                            title: Text(
-                              document['pelanggan'],
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 18,
-                                  color: Colors.white),
-                            ),
-                            subtitle: Text(
-                              controller
-                                  .formatCurrency(document['total_harga']),
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 16,
-                                  color: Colors.white),
-                            ),
-                            trailing: Text(
-                              controller.formatDate(document['tanggal']),
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 16,
-                                  color: Colors.white),
+                              child: ListTile(
+                                onTap: () => Get.toNamed(Routes.HISTORY_INFO,
+                                    arguments: document.id),
+                                title: Text(
+                                  document['pelanggan'],
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                subtitle: Text(
+                                  controller
+                                      .formatCurrency(document['total_harga']),
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 16,
+                                    color: Colors.white70,
+                                  ),
+                                ),
+                                trailing: Text(
+                                  controller.formatDate(document['tanggal']),
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 16,
+                                    color: Colors.white70,
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      } else {
+                        return Center(
+                          child: Text(
+                            'No data',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
                             ),
                           ),
                         );
-                      },
-                    );
-                  } else {
-                    return const Center(
-                      child: Text('No data'),
-                    );
-                  }
-                } else {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              })),
+                      }
+                    } else {
+                      return Center(
+                        child: CircularProgressIndicator(
+                          color: Colors.teal,
+                        ),
+                      );
+                    }
+                  },
+                );
+              }),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(2),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Obx(() {
+                      return Text(
+                        controller.selectedDate.value != null
+                            ? 'Selected Date: ${DateFormat('dd MMMM yyyy', 'id_ID').format(controller.selectedDate.value!)}'
+                            : 'No date selected',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      );
+                    }),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.date_range, color: Colors.black),
+                          onPressed: () => _selectDate(context),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.print, color: Colors.black),
+                          onPressed: () async {
+                            var snapshot =
+                                await controller.streamDataRiwayat().first;
+                            controller.printPdf(snapshot.docs);
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
